c61615ce233f9023ba03ad5baca7ab79182e0312
Riya Bhanot <121608998+RiyaBhanot@users.noreply.github.com>
2024-12-01 11:00:38 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,9 @@
+"use  strict"; //treat all code as newer version of js
+// alert(3+3) we are using nodejs, not browser
+let number = 7
+let name = "nrki"
+let isloggedin = false
+let  temp = null
+let state
+console.log(typeof state)
+console.log(typeof temp)
