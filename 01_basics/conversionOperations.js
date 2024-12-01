6a0b4a37a183ad03de19248d7b9cf2e1bbbc35f8
Riya Bhanot <121608998+RiyaBhanot@users.noreply.github.com>
2024-12-01 12:24:18 +0000

    operations done

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,18 +1,46 @@
 let score = "33"
 let valueInNumber = Number(score)
-console.log(valueInNumber);
+//console.log(valueInNumber);
 let somenumber = 33
 let stringnumber = String(somenumber)
-console.log(stringnumber)
-console.log( typeof stringnumber) // it will show the exact change in datatype
+// console.log(stringnumber)
+// console.log( typeof stringnumber) // it will show the exact change in datatype
 let islogg = 1
 let changetobool = Boolean(islogg)
-console.log(changetobool)
+// console.log(changetobool)
 let islogg2 = "riya" // even if we give string or number and tries to convert it into bool it will give output as true
 let changetobool2 = Boolean(islogg2)
-console.log(changetobool2)
+// console.log(changetobool2)
 let islogg3 = null // even if we give  null or undefined value and tries to convert it into bool it will give output as false
 let changetobool3 = Boolean(islogg3)
-console.log(typeof changetobool3)
+// console.log(typeof changetobool3)
+
+///////////////*******operations ******///////
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+let str1 = "hello"
+let str2 = " riya"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log("1" + "2")
+// console.log(1 + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "2")
+// console.log(true)
+// console.log(+true) // gives 1 as output becuase true is bool and it got incremented so it will become 1  not a good practice
+// console.log(+"") //gives 0
+ 
+let gamecounter = 100
+// gamecounter++
+++gamecounter
+console.log(gamecounter)
+
+
+
+
 
 
