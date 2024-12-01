8c0e9820d072e0f671879c517a7c7671327da2f7
Riya Bhanot <121608998+RiyaBhanot@users.noreply.github.com>
2024-12-01 12:01:39 +0000

    conversionOperations done

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,18 @@
+let score = "33"
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log( typeof stringnumber) // it will show the exact change in datatype
+let islogg = 1
+let changetobool = Boolean(islogg)
+console.log(changetobool)
+let islogg2 = "riya" // even if we give string or number and tries to convert it into bool it will give output as true
+let changetobool2 = Boolean(islogg2)
+console.log(changetobool2)
+let islogg3 = null // even if we give  null or undefined value and tries to convert it into bool it will give output as false
+let changetobool3 = Boolean(islogg3)
+console.log(typeof changetobool3)
+
+
