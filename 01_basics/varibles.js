cee5ec05a0eb6da693e9d015d6068e2c9909e59a
Riya Bhanot <121608998+RiyaBhanot@users.noreply.github.com>
2024-12-01 10:25:07 +0000

    variables done

---
+++ 01_basics/varibles.js
@@ -0,0 +1,9 @@
+const accountId = 145678
+let accountemail = "nrki@gmail.com"
+var acccountpass = "123456"
+accountcity = "bangalore"
+let accountstate;
+
+// accountId = 2
+console.log(accountId)
+console.table([accountId , accountemail, accountstate, acccountpass, accountcity])
