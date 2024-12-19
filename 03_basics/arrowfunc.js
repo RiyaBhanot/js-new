75eed0686f602beb50d55bdd78108b5f20de842a
RiyaBhanot <rajputriya398@gmail.com>
2024-12-19 20:31:26 +0530

    checking for update

--- 03_basics/arrowfunc.js
+++ 03_basics/arrowfunc.js
@@ -16,3 +16,6 @@ console.log(this) // this will give empty string because the context is not defi
 //The this keyword refers to the context where a piece of code, such as a function's body, 
 // is supposed to run. Most typically, it is used in object methods, where this refers to the object that the method is attached to, 
 // thus allowing the same method to be reused on different objects.
+
+// Checking for updates
+fjdfjd
