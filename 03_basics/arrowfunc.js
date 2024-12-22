c294d86cd5debf99544db8e7b1423d3c6d591e94
RiyaBhanot <rajputriya398@gmail.com>
2024-12-22 12:24:09 +0530

    arrow functions done

--- 03_basics/arrowfunc.js
+++ 03_basics/arrowfunc.js
@@ -1,11 +1,10 @@
 const user = {
-    name:"nrki",
-    price : "999",
-    welcomeMsg : function()
-    {
+    name: "nrki",
+    price: "999",
+    welcomeMsg: function () {
         console.log(`${this.name}, welcome to webiste`)
         // console.log(this) // gives the  current context
-        
+
     }
 }
 
@@ -13,7 +12,29 @@ const user = {
 // user.name = "riya"
 // user.welcomeMsg()
 console.log(this) // this will give empty string because the context is not defined 
-//The this keyword refers to the context where a piece of code, such as a function's body, 
-// is supposed to run. Most typically, it is used in object methods, where this refers to the object that the method is attached to, 
+//The this keyword refers to the context where a piece of code, such as a function's body,
+// is supposed to run. Most typically, it is used in object methods, where this refers to the object that the method is attached to,
 // thus allowing the same method to be reused on different objects.
+
+
+//arrow function
+//explicit method of arrow functions
+// const add2 = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// console.log(add2(1, 2))
+
+
+//implicit method of arrow functions
+const add3 = (num1, num2) => (num1 + num2)
+console.log(add3(1, 2))
+
+//method to return an object in arrow functions
+const add4 = (num1, num2) => ({ username: "riya" })
+console.log(add4(1, 2))
+
+//arrow func in loops 
+const arr = [1, 2, 3, 4]
+arr.forEach(() => { }) //elaborating in next video 
 
