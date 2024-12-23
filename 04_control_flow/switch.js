223f9faac739a7c9111aac5b0e3df982651aefb0
RiyaBhanot <rajputriya398@gmail.com>
2024-12-23 19:37:55 +0530

    switch case done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 5
+
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("mar")
+        break;
+    case 4:
+        console.log("apr")
+        break;
+    default:
+        console.log("no")
+        break;
+}
+
+//The switch statement in JavaScript is used to execute one block of code out of many based on the value of an expression.
+// It is a cleaner alternative to multiple if...else conditions, especially when there are many cases to handle.
+
+//default is optional:
+// It acts as a fallback when no cases match.
+// If omitted, and no match is found, nothing happens.
+
+//break
+// The break statement in JavaScript is used to terminate a loop, switch statement, or block of code prematurely.
+// Once break is executed, the program exits the current structure and continues execution at the next line of code after it.
